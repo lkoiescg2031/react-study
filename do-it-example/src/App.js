af1731c79c4a8de2d9d993fce104cff796cdc9a8
taehong <lkoiescg2031@naver.com>
2020-07-29 01:01:34 +0900

    day1 리엑트 시작하기

---
+++ do-it-example/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">두잇! 리액트 시작하기</h1>
+    </div>
+  );
+}
+
+export default App;
