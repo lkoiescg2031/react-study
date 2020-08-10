832af86c23f7643dc6b72d3ccda0c7b76fba99ae
taehong <lkoiescg2031@naver.com>
2020-08-10 23:37:42 +0900

    add all

--- src/stories/WithErrorStory.jsx
+++ src/stories/WithErrorStory.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import input from '../04/InputWithStyles';
 import withError from '../05/withError';
 import Input from '../04/InputWithStyles';
 
-const inputWithError = withError('올바르지 못한 값입니다.')(Input);
+const InputWithError = withError('올바르지 못한 값입니다.')(Input);
 
 storiesOf('WithError', module)
-  .addWithJSX('기본설정', () => <inputWithError name="name" hasError />)
+  .addWithJSX('기본설정', () => <InputWithError name="name" hasError />)
   .addWithJSX('errorMessage 예제', () => (
-    <inputWithError name="name" hasError errorMessage="필수항목입니다." />
+    <InputWithError name="name" hasError errorMessage="필수항목입니다." />
   ));
