51bd9cd40b4c37b898f4bff64305935f7cd30b55
taehong <lkoiescg2031@naver.com>
2020-08-13 02:18:36 +0900

    Add

--- src/06/HomePageWithTwoProvider.jsx
+++ src/06/HomePageWithTwoProvider.jsx
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react';
-import LoadingProviderWithKey from './LoadingProviderWithKey';
 import {
   ButtonWithDefaultLoadingContext,
   ButtonWithLoading2Context,
 } from './ButtonWithLoadingContextAndKey';
+import LoadingProviderWithKey from './LoadingProviderWithKey';
+import ButtonWithConsumer from './ButtonWithConsumer';
 
 const LoadingProvider1 = LoadingProviderWithKey();
 const LoadingProvider2 = LoadingProviderWithKey('loading2');
@@ -14,6 +15,7 @@ function TableComponent() {
         컨텍스트1
       </ButtonWithDefaultLoadingContext>
       <ButtonWithLoading2Context>컨텍스트2</ButtonWithLoading2Context>
+      <ButtonWithConsumer>다중 소비자 예제</ButtonWithConsumer>
     </table>
   );
 }
