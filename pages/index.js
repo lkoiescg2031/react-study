0a927ed1c41838fc0b0ed0c253cb3223f542b866
taehong <lkoiescg2031@naver.com>
2020-09-06 23:36:55 +0900

    Add

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react';
+import MainPage from '../src/08/components/main/MainPage';
+
+export default class IndexDocument extends PureComponent {
+  render() {
+    return <MainPage />;
+  }
+}
